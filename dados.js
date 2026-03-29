b7ff673ea5e22a83c9d11130de975ac67ce6bdc2
Bernardo <bernardogrodrigues@gmail.com>
2026-03-29 19:41:05 -0300

    enviando arquivos

---
+++ dados.js
@@ -0,0 +1,5 @@
+const turmas = {
+  "teste a": ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo", "Fernanda"],
+  "turma b": ["Lucas", "Mariana", "Pedro", "Sofia", "Tiago", "João"],
+  "turma c": ["Arthur", "Beatriz", "Caio", "Diana", "Felipe", "Gabriela"],
+};
